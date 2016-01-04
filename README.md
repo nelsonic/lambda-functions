9622ea6e2c79898988456e520ce23f7bd01b24ec
Benjamin Lees <benji.man.lees@gmail.com>
2016-01-04 12:20:32 +0000

    starting readme

---
+++ README.md
@@ -0,0 +1,9 @@
+## lambda-functions
+
+### Aims
+
+* Step one - Write one single lambda function and deploy using circle-ci.
+
+* Step two - Write anouther lambda function and deploy both using circle-ci.
+
+* Step three - Use circle-ci to deploy lambda functions seperatly.
---
+++ lambda-func-one/index.js
@@ -0,0 +1,16 @@
+var exec = require('child_process').exec;
+
+exports.handler = function(event, context) {
+  if (!event.cmd) {
+    context.fail('update');
+    return;
+  }
+  child = exec(event.cmd, function(error) {
+    // Resolve with result of process
+    context.done(error, 'Process complete!');
+  });
+
+  // Log process stdout and stderr
+  child.stdout.on('data', console.log);
+  child.stderr.on('data', console.error);
+};
